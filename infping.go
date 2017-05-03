c1b2ae9127bf634d4804ba3633ec6d225eee3a1e
Robin <imkwx@imkwx.fr>
2017-05-03 15:07:05 +0200

    Update infping.go

--- infping.go
+++ infping.go
@@ -4,7 +4,7 @@
 package main
 
 import (
-    "github.com/influxdb/influxdb/client"
+    "github.com/influxdata/influxdb/client"
     "github.com/pelletier/go-toml"
     "fmt"
     "log"
@@ -112,7 +112,7 @@ func writePoints(config *toml.TomlTree, con *client.Client, host string, sent st
     bps := client.BatchPoints{
         Points:          pts,
         Database:        db,
-        RetentionPolicy: "default",
+        RetentionPolicy: "fifteen_days",
     }
     _, err := con.Write(bps)
     if err != nil {
