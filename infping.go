b338394248afbd9cee0c7467cd7217ba9a1287ac
Michael Newton <miken32@gmail.com>
2018-03-09 08:58:22 -0800

    Add license and copyright text

--- infping.go
+++ infping.go
@@ -1,5 +1,28 @@
-// infping.go copyright Tor Hveem
-// License: MIT
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Tor Hveem              https://github.com/torhve/infping
+Copyright (c) 2017 Nicholas Van Wiggeren  https://github.com/nickvanw/infping
+Copyright (c) 2018 Michael Newton         https://github.com/miken32/infping
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal 
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 package main
 
@@ -27,7 +50,6 @@ func main() {
 	viper.SetDefault("fping.tos", "0")
 	viper.SetDefault("fping.summary", "10")
 	viper.SetDefault("fping.period", "1000")
-	viper.SetDefault("hosts.hosts", []string{"localhost"})
 	viper.SetDefault("fping.custom", map[string]string{})
 	viper.SetDefault("hosts.hosts", []string{"localhost"})
 
